eb4734e5cf0c0977ca43317c258ff0aaeb498eea
Jakob Schloer <jakob.schloer@gmail.com>
2023-08-19 17:48:48 +0200

    Change router.

--- src/components/Router.vue
+++ src/components/Router.vue
@@ -3,10 +3,10 @@
     <div id="nav">
       <router-link to="/">Home</router-link> -
       <!-- <router-link to="/about">About</router-link> - -->
-      <router-link to="/experience">CV</router-link> - 
-      <router-link to="/projects">Projects</router-link> -
-      <router-link to="/talks">Talks</router-link> -
-      <router-link to="/hobbies">Others</router-link>
+      <!-- <router-link to="/experience">CV</router-link> - -->
+      <!-- <router-link to="/projects">Projects</router-link> - -->
+      <!-- <router-link to="/talks">Talks</router-link> - -->
+      <!-- <router-link to="/hobbies">Others</router-link> -->
     </div>
   </div>
 </template>
