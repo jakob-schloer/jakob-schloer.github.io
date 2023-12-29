bf1c49c2485d178d4aa8bf229380180b95941176
Jakob Schloer <jakob.schloer@gmail.com>
2023-12-29 12:10:52 +0100

    Update wedding page.

--- src/views/Wedding.vue
+++ src/views/Wedding.vue
@@ -61,25 +61,36 @@ export default {
         { time: 'Ab 15:30', activity: 'Feier im Freistil in Tübingen' },
         // Add more events as needed
       ],
-      others: `Bezüglich des Programms und anderen Überraschungen wendet euch bitte an die Trauzeugen: <br> 
+      others: `<b>Trauzeugen</b> <br>
+                Bezüglich des Programms und anderen Überraschungen wendet euch bitte an die Trauzeugen: <br> 
                 Moritz Frankerl <a href="mailto:moritz.frankerl@proton.me">(moritz.frankerl@proton.me) </a> <br> 
                 Hannah Weissinger <a href="mailto:hannah.weissinger@gmail.com">(hannah.weissinger@gmail.com) </a> <br> <br> 
-                Wer es nach der durchtanzten Nacht nicht weit haben möchte, 
-                kann im <a href="https://all.accor.com/hotel/9841/index.de.shtml">Ibis Hotel</a> 
-                um die Ecke oder in der <a href="https://www.jugendherberge.de/jugendherbergen/tuebingen/">JuHe</a> 
-                übernachten. <br> <br>
+                
+                <b>Geschenke</b> <br>
                 In unserer WG haben wir schon genug Küchengeräte, wir freuen uns daher über 
                 finanzielle Unterstützung oder Spenden an: 
                 <a href="www.givinggreen.earth">www.givinggreen.earth</a> 
                 oder <a href="www.climateemergencyfund.or">www.climateemergencyfund.or</a> <br> <br>
 
+                <b>Anreise</b> <br>
+                Sowohl, das Freistil als auch das Rathaus sind in laufnähe vom Bahnhof. <br>
                 Falls ihr mit dem Auto anreist, könnt ihr im <a href="https://www.google.com/maps?ll=48.518055,9.061029&z=18&t=m&hl=en&gl=DE&mapclient=embed&cid=4585157566754435213">Neckar-Parkhaus</a>
-                nebenan parken. 
+                direkt neben dem Freistil parken und von dort aus zu Fuß zum Standesamt kommen. <br> <br>
+
+                <b>Dresscode</b> <br>
+                Kommt, wie ihr seid – nur ein bisschen schicker. 
                 `,
       mapUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10571.218644826758!2d9.061115!3d48.5178092!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4799fab9f6b6598b%3A0xf4fa6086a181b992!2sFreistil%20Taproom!5e0!3m2!1sen!2sde!4v1694983104196!5m2!1sen!2sde"
     };
   },
 };
+
+/** 
+Wer es nach der durchtanzten Nacht nicht weit haben möchte, 
+kann im <a href="https://all.accor.com/hotel/9841/index.de.shtml">Ibis Hotel</a> 
+um die Ecke oder in der <a href="https://www.jugendherberge.de/jugendherbergen/tuebingen/">JuHe</a> 
+übernachten. <br> <br>
+**/
 </script>
 
 
